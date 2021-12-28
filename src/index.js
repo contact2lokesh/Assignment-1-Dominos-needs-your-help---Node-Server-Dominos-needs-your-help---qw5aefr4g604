2d00302b20ce163f3339bfbfa6274b8c28d12fb2
contact2lokesh <contacttolokesh22.com>
2021-12-28 13:28:55 +0530

    welcome changes

--- src/index.js
+++ src/index.js
@@ -5,24 +5,25 @@ const httpServer = http.createServer(handleServer);
 
 function handleServer(req, res) {
   if(req.url == '/'){
-      res.writeHead(200, {"content-type": "text/plain"});
+      res.writeHead(200, {'content-type': 'text/plain'});
       //set response content
-      res.write("This is Home Page");
+      res.write('This is Home Page');
       res.end();
   } 
     else if(req.url == '/welcome'){
-     res.writeHead(200, {"content-type": "text/pain"});
-     res.write("Welcome to Dominos!");
+     res.writeHead(200, { 'content-type': 'text/plain'});
+     res.write('Welcome to Dominos!');
      res.end();
     }
 
 
+
     
-    // else {
-    //     res.writeHead(404, {"content-type": "text/html"});
-    //     res.write("<h1>Invalid Request</h1>");
-    //     res.end();
-    // }
+    else {
+        res.writeHead(404, {"content-type": "text/html"});
+        res.write("<h1>Invalid Request</h1>");
+        res.end();
+    }
 }
 
 httpServer.listen(8081);
