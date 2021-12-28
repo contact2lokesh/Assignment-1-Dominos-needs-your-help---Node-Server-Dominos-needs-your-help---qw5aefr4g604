c0fc4bde1a2c11012c7dd765eae750b9371008f2
contact2lokesh <contacttolokesh22.com>
2021-12-28 13:18:59 +0530

    welcome route and 404 error

--- src/index.js
+++ src/index.js
@@ -4,7 +4,28 @@ const httpServer = http.createServer(handleServer);
 
 
 function handleServer(req, res) {
-  
+  if(req.url == '/'){
+      res.writeHead(200, {"content-type": "text/plain"});
+      //set response content
+      res.write("This is Home Page");
+      res.end();
+  } 
+    else if(req.url == '/welcome'){
+     res.writeHead(200, {"content-type": "text/pain"});
+     res.write("Welcome to Dominos!");
+     res.end();
+    }
+
+
+    
+    else {
+        res.writeHead(404, {"content-type": "text/html"});
+        res.write("<h1>Invalid Request</h1>");
+        res.end();
+    }
 }
+
+httpServer.listen(8081);
+console.log('Node.js web server at port 8081 is running..')
 
 module.exports = httpServer;
