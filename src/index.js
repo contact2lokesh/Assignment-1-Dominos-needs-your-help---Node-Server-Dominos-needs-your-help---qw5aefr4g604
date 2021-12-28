d83a502be30d4dddc7f34477e3897766d8e58281
contact2lokesh <contacttolokesh22.com>
2021-12-28 14:28:18 +0530

    contact response

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ var http = require("http");
 
 const httpServer = http.createServer(handleServer);
 
+const objData =   {
+                      phone: '18602100000',
+                      email: 'guestcaredominos@jublfood.com'
+                   }
+                   const jsonContent = JSON.stringify(objData);
+                //    const objContent = JSON.parse(jsonContent);
+
 
 function handleServer(req, res) {
   if(req.url == '/'){
@@ -15,7 +22,11 @@ function handleServer(req, res) {
      res.write('Welcome to Dominos!');
      res.end();
     }
-
+     else if(req.url == '/contact'){
+         res.writeHead(200, {'content-type': 'application/json'});
+         res.write(jsonContent);
+         res.end();
+     }
 
 
     
