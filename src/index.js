81bce33d51cd0d273dd3e9bc604da6d5066cc690
contact2lokesh <contacttolokesh22.com>
2021-12-28 14:46:04 +0530

    404 created empty object

--- src/index.js
+++ src/index.js
@@ -27,16 +27,16 @@ function handleServer(req, res) {
          res.write(jsonContent);
          res.end();
      }
-      else if(req.url !== '/contact'){
-        res.writeHead(404, {'content-type': 'application/json'});
-        res.write("Invalid");
-        res.end();
-      }
+    //   else if(req.url !== '/contact'){
+    //     res.writeHead(404, {'content-type': 'application/json'});
+    //     res.write("Invalid");
+    //     res.end();
+    //   }
 
     
      else {
-        res.writeHead(404, {"content-type": "text/html"});
-        res.write("Invalid Request");
+        res.writeHead(404, {"content-type": 'application/json'});
+        res.write(`${{}}`);
         res.end();
     } 
 }
